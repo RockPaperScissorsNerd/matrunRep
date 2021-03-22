6903432f2585486a6cbb0961a4d6638f5514f902
Mathem <mathiashem@viken.no>
2021-03-22 13:45:24 +0100

    RPS Structure 2

--- MathiasOgRune/RPS.aspx.cs
+++ MathiasOgRune/RPS.aspx.cs
@@ -30,11 +30,6 @@ namespace MathiasOgRune
             Response.Redirect("Default.aspx");
         }
 
-        protected void StartGame(object sender, EventArgs e)
-        {
-            
-        }
-
         protected void RPSPick(object sender, EventArgs e)
         {
             Button RPS = (Button)sender;
@@ -45,14 +40,17 @@ namespace MathiasOgRune
                 if (BotRPS == 1)
                 {
                     //STALEMATE (ROCK VS ROCK)
+                    RPSPicked.Text = "You picked: " + RPS.ID + " and the bot picked Rock. STALEMATE!";
                 }
                 else if (BotRPS == 2)
                 {
                     //BOT WINS (ROCK VS PAPER)
+                    RPSPicked.Text = "You picked: " + RPS.ID + " and the bot picked Paper. BOT WINS!";
                 }
                 else if (BotRPS == 3)
                 {
                     //PLAYER WINS (ROCK VS SCISSORS)
+                    RPSPicked.Text = "You picked: " + RPS.ID + " and the bot picked Scissors. YOU WIN!";
                 }
             }
             else if (RPS.ID == "Paper")
@@ -60,14 +58,17 @@ namespace MathiasOgRune
                 if (BotRPS == 1)
                 {
                     //PLAYER WINS (PAPER VS ROCK)
+                    RPSPicked.Text = "You picked: " + RPS.ID + " and the bot picked Rock. YOU WIN!";
                 }
                 else if (BotRPS == 2)
                 {
                     //STALEMATE (PAPER VS PAPER)
+                    RPSPicked.Text = "You picked: " + RPS.ID + " and the bot picked Paper. STALEMATE!";
                 }
                 else if (BotRPS == 3)
                 {
                     //BOT WINS (PAPER VS SCISSORS)
+                    RPSPicked.Text = "You picked: " + RPS.ID + " and the bot picked Scissors. BOT WINS!";
                 }
             }
             else if (RPS.ID == "Scissors")
@@ -75,14 +76,17 @@ namespace MathiasOgRune
                 if (BotRPS == 1)
                 {
                     //BOT WINS (SCISSORS VS ROCK)
+                    RPSPicked.Text = "You picked: " + RPS.ID + " and the bot picked Rock. BOT WINS!";
                 }
                 else if (BotRPS == 2)
                 {
                     //PLAYER WINS (SCISSORS VS PAPER)
+                    RPSPicked.Text = "You picked: " + RPS.ID + " and the bot picked Paper. YOU WIN!";
                 }
                 else if (BotRPS == 3)
                 {
                     //STALEMATE (SCISSORS VS SCISSORS)
+                    RPSPicked.Text = "You picked: " + RPS.ID + " and the bot picked Scissors. STALEMATE!";
                 }
             }
         }
